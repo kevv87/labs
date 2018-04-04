78e82896192e6c40ea7afbe86ba43c40ffda2143
¨Kevin¨ <magneticok@gmail.com>
2018-04-04 10:34:10 -0600

    Terminado el codigo de pila y cola

---
+++ lab1/cola.py
@@ -0,0 +1,31 @@
+import time  # Modulo necesario para tomar el tiempo
+
+llamadas = 0  # Contador para llamadas recursivas
+
+
+# E: Un numero n.
+# S: La n posicion en la sucesion de fibonacci.
+# R: N debe ser entero positivo
+def fib(n):
+    if isinstance(n, int):
+        return fib_aux(n, 0, 1)
+    else:
+        return "Error"
+
+
+def fib_aux(n, a,  b):
+    global llamadas
+    llamadas = llamadas + 1
+    if n == 0:  # Caso base 1
+        return a
+    elif n == 1:  # Caso base 2
+        return b
+    return fib_aux(n-1, b, a+b)
+
+
+start = time.clock()  # Iniciando el cronometro
+# print (fib(35))
+end = time.clock()  # Deteniendo el cronometro
+
+print(llamadas)
+print(end-start)
---
+++ lab1/pila.py
@@ -0,0 +1,31 @@
+import time
+llamadas = 0  # Contador para llamadas recursivas
+
+
+# E: Un numero n.
+# S: La n posicion en la sucesion de fibonacci.
+# R: N debe ser entero positivo
+def fib(n):
+    if isinstance(n, int) and n >= 0:
+        return fib_aux(n)
+    else:
+        return 'Err'
+
+
+def fib_aux(n):
+    global llamadas
+    if n == 0:  # Caso base 1
+        return 0
+    elif n <= 2:  # Caso base 2
+        return 1
+    else:  # Llamadas recursivas
+        llamadas += 2
+        return fib(n-1) + fib(n-2)
+
+
+start = time.clock()  # Iniciando el cronometro
+print(fib(35))
+end = time.clock()  # Parando el cronometro
+
+print(llamadas)
+print(end-start)
