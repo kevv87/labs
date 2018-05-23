09974c8f9cf82e74f92a80ea278428d541bcec6e
kevv87 <7m6JHC84iMsw>
2018-05-23 09:10:05 -0600

    Modificado reve

--- lab3/ListaD.py
+++ lab3/ListaD.py
@@ -35,13 +35,13 @@ class ListaDoble:
 
     def reve(self):
         nodo = self.tail
-        while nodo != None:
-            nodo.next = nodo.prev
-            nodo = nodo.next
-        save = self.head
-        self.head = self.tail
-        self.tail = save
-        self.norm()
+        print('[', end='')
+        while nodo != None and nodo != self.head:
+            print(nodo.valor, end=', ')
+            nodo = nodo.prev
+        if nodo != None:
+            print(nodo.valor, end='')
+        print(']')
 
     def toList(self):
         nodo = self.head
@@ -62,28 +62,33 @@ class ListaDoble:
         nodo = self.head
         cont = 0
         while nodo != None and cont == 0:
-            if nodo.value() == ele and nodo == self.head:
+            if nodo.valor == ele and nodo == self.head:
                 self.head = nodo.next
                 self.largo -= 1
                 cont += 1
-            elif nodo.next.value() == ele and nodo.next == self.tail:
+            elif nodo.next.valor == ele and nodo.next == self.tail:
                 self.largo -= 1
                 self.tail = nodo
                 nodo.next = None
                 cont += 1
-            elif nodo.next.value() == ele:
+            elif nodo.next.valor == ele:
                 nodo.next = nodo.next.next
                 cont += 1
             nodo = nodo.next
 
 def prueba():
     lista = ListaDoble()
-    lista.appe(12)
-    lista.appe(15)
-    lista.appe(13)
+    empty = ListaDoble()
+    for i in range(10):
+        lista.appe(i)
+    lista.appe(7)
     lista.printL()
     lista.reve()
     lista.printL()
-
+    lista.dele(7)
+    lista.dele(0)
+    lista.dele(5)
+    empty.dele(1)
+    print(lista.toList())
 
 prueba()
